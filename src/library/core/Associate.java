612f15e9dd73893fd13f185be48253eebfce4f46
Igor Marques da Silva <igormarquessilva@gmail.com>
2013-03-15 15:30:06 -0300

    Adicionando classes centrais (ainda incompletas

---
+++ src/library/core/Associate.java
@@ -0,0 +1,15 @@
+package library.core;
+
+public class Associate {
+	private String name;
+	//criar vetor de livros
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
---
+++ src/library/core/Book.java
@@ -0,0 +1,36 @@
+package library.core;
+
+public class Book {
+	
+	private String title;
+	private int total_amount;
+	private int lent_amount;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public int getTotal_amount() {
+		return total_amount;
+	}
+	public void setTotal_amount(int total_amount) {
+		this.total_amount = total_amount;
+	}
+	
+	public int getLent_amount() {
+		return lent_amount;
+	}
+	public void setLent_amount(int lent_amount) {
+		this.lent_amount = lent_amount;
+	}
+	
+	public int stock_amount(){
+		//return self.getTotal_amount() - self.getLent_amount();
+		return 1;
+	}
+	
+
+}
