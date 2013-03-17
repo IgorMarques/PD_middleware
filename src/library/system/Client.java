aa6e9c63ef40cfd465181d6066abc1ab1568f89b
Igor Marques da Silva <igormarquessilva@gmail.com>
2013-03-17 11:47:04 -0300

    Classe cliente criada

---
+++ src/library/system/Client.java
@@ -0,0 +1,16 @@
+package library.system;
+
+import library.core.Librarian;
+
+public class Client {
+	private Librarian librarian;
+
+	public Librarian getLibrarian() {
+		return librarian;
+	}
+
+	public void setLibrarian(Librarian librarian) {
+		this.librarian = librarian;
+	}
+
+}
